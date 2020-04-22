821cc6d7f74d731753ead631131cb5bf21b633c1
yoshi-automation <yoshi-automation@google.com>
2020-04-22 05:25:20 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- google/cloud/trace_v1/gapic/enums.py
+++ google/cloud/trace_v1/gapic/enums.py
@@ -25,14 +25,50 @@ class ListTracesRequest(object):
         Type of data returned for traces in the list.
 
         Attributes:
-          VIEW_TYPE_UNSPECIFIED (int): Default is ``MINIMAL`` if unspecified.
+          VIEW_TYPE_UNSPECIFIED (int): See ``HttpRule``.
           MINIMAL (int): Minimal view of the trace record that contains only the project
           and trace IDs.
           ROOTSPAN (int): Root span view of the trace record that returns the root spans along
           with the minimal trace data.
-          COMPLETE (int): Complete view of the trace record that contains the actual trace data.
-          This is equivalent to calling the REST ``get`` or RPC ``GetTrace``
-          method using the ID of each listed trace.
+          COMPLETE (int): If this SourceCodeInfo represents a complete declaration, these are
+          any comments appearing before and after the declaration which appear to
+          be attached to the declaration.
+
+          A series of line comments appearing on consecutive lines, with no other
+          tokens appearing on those lines, will be treated as a single comment.
+
+          leading_detached_comments will keep paragraphs of comments that appear
+          before (but not connected to) the current element. Each paragraph,
+          separated by empty lines, will be one comment element in the repeated
+          field.
+
+          Only the comment content is provided; comment markers (e.g. //) are
+          stripped out. For block comments, leading whitespace and an asterisk
+          will be stripped from the beginning of each line other than the first.
+          Newlines are included in the output.
+
+          Examples:
+
+          optional int32 foo = 1; // Comment attached to foo. // Comment attached
+          to bar. optional int32 bar = 2;
+
+          optional string baz = 3; // Comment attached to baz. // Another line
+          attached to baz.
+
+          // Comment attached to qux. // // Another line attached to qux. optional
+          double qux = 4;
+
+          // Detached comment for corge. This is not leading or trailing comments
+          // to qux or corge because there are blank lines separating it from //
+          both.
+
+          // Detached comment for corge paragraph 2.
+
+          optional string corge = 5; /\* Block comment attached \* to corge.
+          Leading asterisks \* will be removed. */ /* Block comment attached to \*
+          grault. \*/ optional int32 grault = 6;
+
+          // ignored detached comments.
         """
 
         VIEW_TYPE_UNSPECIFIED = 0
